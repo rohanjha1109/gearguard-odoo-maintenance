edbeac072922f121ef2bb7e2b7a2c6b3bb3c4a64
Rohan Jha <rohanjha369@gmail.com>
2025-12-27 12:13:15 +0530

    Initial project setup for GearGuard Odoo module

---
+++ gear_guard/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "GearGuard - Maintenance Tracker",
+    "version": "1.0",
+    "category": "Maintenance",
+    "summary": "Odoo maintenance management module for hackathon",
+    "depends": ["base"],
+    "data": [],
+    "application": True,
+}
